2c39a4cf1ba2c7acb3332ef287c945c2ab0e61f1
Oakley-1229 <october856711@gmail.com>
2025-06-02 10:53:21 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/x03gW',
+	'https://www.surveycake.com/s/pGqPG',
 ];
