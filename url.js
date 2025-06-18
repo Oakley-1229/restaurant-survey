3dade8c54f93580abf0ee986a470c0fbde599c86
Oakley-1229 <october856711@gmail.com>
2025-06-18 17:36:25 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,6 +8,8 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/x03gW',
-	'https://www.surveycake.com/s/pGqPG',
+	'https://www.surveycake.com/s/VGrV9',
+	'https://www.surveycake.com/s/GQeK0',
+	'https://www.surveycake.com/s/7wgKq',
+	'https://www.surveycake.com/s/dPKM2',
 ];
